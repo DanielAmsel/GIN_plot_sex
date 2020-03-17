7a56c16eda717ab8fdde1ba5405545e90a6bf8f7
Daniel Amsel <daniel.amsel@patho.med.uni-giessen.de>
2020-03-17 15:13:55 +0100

    changed vals to Inf again

--- plot_sex.R
+++ plot_sex.R
@@ -8,7 +8,7 @@ if (length(args)==1) {
   stop("Please provide only one input file.n", call.=FALSE)
 }
 #for testing
-#input_file = "test_sex.csv"
+input_file = "/mnt/STORAGE/workdir/GIN_plot_sex/example/test_sex.csv"
 
 # expect output from: https://github.com/edawson/check-sex
 # looks like: 
@@ -19,11 +19,11 @@ sex_value <- sex_table$V3
 
 d <- data.frame(gender =c(0,1,2,3,4,5,6,7,8),patient=1)
 
-male <- data.frame(xmin=0.9, xmax=1.5, ymin=0, ymax=2)
-female <- data.frame(xmin=4.5, xmax=7.0, ymin=0, ymax=2)
+male <- data.frame(xmin=0.9, xmax=1.5, ymin=-Inf, ymax=Inf)
+female <- data.frame(xmin=4.5, xmax=7.0, ymin=-Inf, ymax=Inf)
 
 
-figure <- ggplot(d, aes(x=8, y=2))+
+figure <- ggplot(d, aes(x=Inf, y=Inf))+
   geom_point(aes(x=sex_value, y=1), colour="black", size=5)+ 
   geom_rect(data=male,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),alpha=0.1)+
   geom_rect(data=female,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),alpha=0.1)+
