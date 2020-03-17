b232a50195c7e369918d15f01137b6dbca4cdf4a
Daniel Amsel <daniel.amsel@patho.med.uni-giessen.de>
2020-03-17 15:09:33 +0100

    moved example files and replaced Inf with vals in script

--- plot_sex.R
+++ plot_sex.R
@@ -7,8 +7,9 @@ if (length(args)==1) {
 } else  {
   stop("Please provide only one input file.n", call.=FALSE)
 }
+#for testing
+#input_file = "test_sex.csv"
 
-input_file = "test_sex.csv"
 # expect output from: https://github.com/edawson/check-sex
 # looks like: 
 # X / Y ratio : 0.9917 
@@ -16,13 +17,13 @@ sex_table <- read.csv(input_file, header=FALSE, sep = " ")
 sex_value <- sex_table$V3
 
 
-d <- data.frame(gender =c(0,1,2,3,4,5,6,7,8),patient=Inf)
+d <- data.frame(gender =c(0,1,2,3,4,5,6,7,8),patient=1)
 
-male <- data.frame(xmin=0.9, xmax=1.5, ymin=-Inf, ymax=Inf)
-female <- data.frame(xmin=4.5, xmax=7.0, ymin=-Inf, ymax=Inf)
+male <- data.frame(xmin=0.9, xmax=1.5, ymin=0, ymax=2)
+female <- data.frame(xmin=4.5, xmax=7.0, ymin=0, ymax=2)
 
 
-figure <- ggplot(d, aes(x=Inf, y=Inf))+
+figure <- ggplot(d, aes(x=8, y=2))+
   geom_point(aes(x=sex_value, y=1), colour="black", size=5)+ 
   geom_rect(data=male,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),alpha=0.1)+
   geom_rect(data=female,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),alpha=0.1)+
@@ -30,5 +31,6 @@ figure <- ggplot(d, aes(x=Inf, y=Inf))+
   xlab("sex prediction")+
   scale_y_continuous(name="male", breaks=NULL, sec.axis = sec_axis(~., name = "female", breaks=NULL))+
   theme_minimal()
-
+figure
 ggsave("plot.png", width=100, height=25, units="mm")
+
